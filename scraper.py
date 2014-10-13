8aa0ee0da29e82cac9eec715cb95315e75347b2e
David Marin <dm@davidmarin.org>
2014-10-13 12:04:20 -0700

    can now specify urls on command line or through $MORPH_URLS

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import logging
 from argparse import ArgumentParser
 from httplib import BadStatusLine
+from os import environ
 from traceback import print_exc
 from urllib2 import HTTPError
 from urllib2 import URLError
@@ -58,23 +59,28 @@ def main():
 
     log_to_stderr(verbose=opts.verbose, quiet=opts.quiet)
 
-    all_urls = set()
+    if opts.urls:
+        all_urls = opts.urls
+    elif environ.get('MORPH_URLS'):
+        all_urls = filter(None, environ['MORPH_URLS'].split())
+    else:
+        all_urls = set()
 
-    for db_name in SOURCE_DBS:
-        download_db(db_name)
-        db = open_db(db_name)
-        for table in show_tables(db):
-            if table in SKIP_TABLES:
-                continue
-            urls = select_urls(db, table)
-            if urls:
-                log.info('read {} urls from {}.{}'.format(
-                    len(urls), db_name, table))
-            all_urls.update(urls)
+        for db_name in SOURCE_DBS:
+            download_db(db_name)
+            db = open_db(db_name)
+            for table in show_tables(db):
+                if table in SKIP_TABLES:
+                    continue
+                urls = select_urls(db, table)
+                if urls:
+                    log.info('read {} urls from {}.{}'.format(
+                        len(urls), db_name, table))
+                all_urls.update(urls)
 
     dt = open_dt()
-    http_failures = []  # tuple of (url, failure_desc)
-    other_failures = []  # tuple of (url, failure_desc)
+    http_failures = []  # tuple of (url, exception)
+    other_failures = []  # tuple of (url, exception)
 
     for i, url in enumerate(sorted(all_urls)):
         log.info('scraping {} ({} of {})'.format(
@@ -91,18 +97,18 @@ def main():
                 html = scrape(url)
             # we want to know if URL no longer works
             except HTTPError as e:
-                http_failure = (url, str(e.code))
+                http_failure = (url, e)
                 # track permanent failures in DB
                 if e.code in PERMANENT_FAILURES:
                     row = dict(url=url, http_status=e.code)
                     dt.upsert(row, 'url')
             except URLError as e:
-                http_failure = (url, str(e.reason))
+                http_failure = (url, e)
             except BadStatusLine as e:
-                http_failure = (url, str(e))
+                http_failure = (url, e)
 
             if http_failure:
-                log.warn('  {}'.format(http_failure[1]))
+                log.warn('  {}'.format(str(http_failure[1])))
                 continue
 
             if html:
@@ -116,22 +122,22 @@ def main():
 
         except Exception as e:
             # other failures are more serious; print the full exception
-            other_failures.append(url, str(e))
+            other_failures.append((url, str(e)))
             print_exc()
 
     # show a summary of failures
     if http_failures:
         log.warn('HTTP error on {} URL{}:'.format(
             len(http_failures), 's' if len(http_failures) > 2 else ''))
-        for url, desc in http_failures:
-            log.warn(u'{} ({})'.format(url, desc))
+        for url, e in http_failures:
+            log.warn(u'{} ({})'.format(url, first_line(str(e))))
 
     if other_failures:
         raise Exception(
             'unexpected error on {} URL{}:\n'.format(
                 len(other_failures), 's' if len(other_failures) > 2 else ''),
-                '\n'.join(u'{} ({})'.format(url, desc)
-                          for url, desc in other_failures))
+                '\n'.join(u'{} ({})'.format(url, first_line(str(e)))
+                          for url, e in other_failures))
 
     total_failures = len(http_failures) + len(other_failures)
     if total_failures > len(all_urls) * MAX_PROPORTION_FAILURES:
@@ -142,6 +148,8 @@ def main():
 
 def parse_args(args=None):
     parser = ArgumentParser()
+    parser.add_argument('urls', metavar='N', nargs='*',
+                        help='urls to scrape')
     parser.add_argument(
         '-v', '--verbose', dest='verbose', default=False, action='store_true',
         help='Enable debug logging')
@@ -150,6 +158,10 @@ def parse_args(args=None):
         help='Turn off info logging')
 
     return parser.parse_args(args)
+
+
+def first_line(s):
+    return s.split('\n')[0]
 
 
 def select_urls(db, table):
